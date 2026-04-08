b0c6f79d8a58654e9f99ce9a5aa883fe46b58606
luciadelama <delamadridlucia@gmail.com>
2026-04-08 14:20:18 +0200

    form + steps in media

--- frontend/src/sections/Steps/Steps.jsx
+++ frontend/src/sections/Steps/Steps.jsx
@@ -20,9 +20,9 @@ const Steps = () => {
                     <p>Complete the submission form with:</p>
                     <ul className="list">
                         <li>Gene name</li>
-                        <li><b>MANE</b> transcript reference</li>
+                        <li>MANE transcript reference</li>
                         <li>Codon-level variant details</li>
-                        <li>Genome build <b>hg38</b></li>
+                        <li>Genome build hg38</li>
                         <li>Contact information</li>
                     </ul>
                     <p>Accurate transcript and codon information are essential for assay design.</p>
@@ -40,11 +40,8 @@ const Steps = () => {
                 <div className="circle">3</div>
                 <div className="step-content">
                     <h2>Functional Assessment</h2>
-                    <p>Variants are experimentally evaluated in controlled 
-                        <br />cellular systems to determine functional impact.
-                        <br />
-                        <br />
-                        <b>Turnaround Time</b>
+                    <p>Variants are experimentally evaluated in controlled cellular systems to determine functional impact.
+                        <br /><b>Turnaround Time</b>
                     </p>
                     <ul className="list">
                         <li>CRISPR-Select assay: 6–8 weeks</li>
@@ -56,16 +53,14 @@ const Steps = () => {
                 <div className="circle">4</div>
                 <div className="step-content">
                     <h2>Track Progress</h2>
-                    <p>Upon submission, a tracking ID is issued. Progress can be 
-                        <br />monitored via the “Track Your Order” function on the website.</p>
+                    <p>Upon submission, a tracking ID is issued. Progress can be monitored via the “Track Your Order” function on the website.</p>
                 </div>
             </div>
             <div className="step">
                 <div className="circle">5</div>
                 <div className="step-content">
                     <h2>Reporting</h2>
-                    <p>A final report summarizing functional results will be send by 
-                        <br />email to requester and subsequently uploaded in ClinVar. </p>
+                    <p>A final report summarizing functional results will be send by email to requester and subsequently uploaded in ClinVar. </p>
                 </div>
             </div>
         </div>
